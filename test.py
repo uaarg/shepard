274ee4633d6d9f255da5c657f71282ae42c28b24
Liyu2001 <liyu@ualberta.ca>
2022-11-01 17:37:33 +0000

    modified test.py argument parsing

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from dronekit import connect, VehicleMode
 from time import sleep
 
 parser = argparse.ArgumentParser(description='Establish MavLink Connection')
-parser.add_argument('master', type=str, nargs='+', default='127.0.0.1:14550', help='port for MavLink connection')
-parser.add_argument('--wait_ready', type=bool, nargs=1, default=False, const=True,
+parser.add_argument('--master', type=str, nargs='?', default='127.0.0.1:14550', help='port for MavLink connection')
+parser.add_argument('--wait_ready', nargs='?', type=bool, default=False, const=True,
                     help='whether to wait for attribute download')
 
 args = parser.parse_args()
@@ -19,7 +19,7 @@ while True:
 
     if roll < 0 and mode != VehicleMode('MANUAL'):
         vehicle.mode = VehicleMode('MANUAL')
-    elif mode != VehicleMode('RTL'):
+    elif roll>0 and mode != VehicleMode('RTL'):
         vehicle.mode = VehicleMode('RTL')
 
     sleep(1)
