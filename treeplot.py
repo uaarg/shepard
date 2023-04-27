0ac7eb737397dd828e7e5fa6f38e327b3e25c219
Uzair Tariq <uzair0213@icloud.com>
2023-04-26 19:47:09 -0600

    changed data source. formatting

--- treeplot.py
+++ treeplot.py
@@ -4,29 +4,29 @@ import tree
 
 
 # CHANGE THESE TO CHANGE PARAMETERS OF PLOT
-DATA_SOURCE = "randomdata.csv"
+DATA_SOURCE = "data2.csv"
 INTERSECT_SAFE = True
 
 # color variables
-res_col = 'darkorange'
-waypoint_col = 'royalblue'
-start_col = 'tomato'
-target_col = 'limegreen'
+res_col = "darkorange"
+waypoint_col = "royalblue"
+start_col = "tomato"
+target_col = "limegreen"
 
 # get all required data
 parsed_nodes = tree.parse_data(DATA_SOURCE)
 waypoints, nofly_region, margin_region = tree.create_safe_waypoints(parsed_nodes)
 
 # original nodes
-points_x = [i[0] for i in parsed_nodes.values() if i[2] == 'obstacle']
-points_y = [i[1] for i in parsed_nodes.values() if i[2] == 'obstacle']
+points_x = [i[0] for i in parsed_nodes.values() if i[2] == "obstacle"]
+points_y = [i[1] for i in parsed_nodes.values() if i[2] == "obstacle"]
 # print(f"{points_x = }\n{points_y = }")
 
 for node in parsed_nodes.values():
-    if node[2] == 'start':
+    if node[2] == "start":
         start_x = node[0]
         start_y = node[1]
-    elif node[2] == 'target':
+    elif node[2] == "target":
         target_x = node[0]
         target_y = node[1]
 
@@ -38,7 +38,7 @@ names = [i for i in waypoints.keys()]
 for i in range(len(waypoints)):
     x = safe_x[i]
     y = safe_y[i]
-    plt.plot(x, y, marker='^', mfc=waypoint_col, mec=waypoint_col)
+    plt.plot(x, y, marker="^", mfc=waypoint_col, mec=waypoint_col)
     plt.text(x + 0.1, y + 0.1, names[i])
 
 # polygon sides
@@ -53,7 +53,9 @@ for side in nofly_region.sides:
 
 # valid paths
 # Set intersect_safe to either True or False to see both options.
-connections, coords = tree.find_valid_connections(waypoints, nofly_region, margin_region, intersect_safe=INTERSECT_SAFE)
+connections, coords = tree.find_valid_connections(
+    waypoints, nofly_region, margin_region, intersect_safe=INTERSECT_SAFE
+)
 # print(f"{connections = }\n{coords = }")
 for c in coords:
     paths_x = []
@@ -62,11 +64,11 @@ for c in coords:
     paths_y.append(c[0][1])
     paths_x.append(c[1][0])
     paths_y.append(c[1][1])
-    plt.plot(paths_x, paths_y, linestyle=':', color='gray')
+    plt.plot(paths_x, paths_y, linestyle=":", color="gray")
 
 # plot
-plt.plot(points_x, points_y, marker='o', mfc=res_col, mec=res_col, linestyle='None')
-plt.plot(start_x, start_y, marker='X', mfc=start_col, mec=start_col)
-plt.plot(target_x, target_y, marker='X', mfc=target_col, mec=target_col)
-plt.axis('square')
+plt.plot(points_x, points_y, marker="o", mfc=res_col, mec=res_col, linestyle="None")
+plt.plot(start_x, start_y, marker="X", mfc=start_col, mec=start_col)
+plt.plot(target_x, target_y, marker="X", mfc=target_col, mec=target_col)
+plt.axis("square")
 plt.show()
