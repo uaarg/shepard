597d0b1bdc6f5f75aa5e3c6130016b8d4627e337
Uzair Tariq <uzair0213@icloud.com>
2023-05-04 21:02:59 -0400

    Creates draft email now

--- email/gcloudemail.py
+++ email/gcloudemail.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 
 import os.path
 
+import base64
+from email.message import EmailMessage
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -9,7 +12,39 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.addons.current.action.compose"]
+SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
+
+
+def create_draft_email(service, message_text):
+    try:
+        message = EmailMessage()
+        message.set_content(message_text)
+        message["To"] = "umtariq@ualberta.ca"
+        message["From"] = "dev.uaarg@gmail.com"
+        message["Subject"] = "Email API Test"
+
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+        create_message = {"message": {"raw": encoded_message}}
+
+        draft = (
+            service.users().drafts().create(userId="me", body=create_message).execute()
+        )
+
+        print(f'Draft id: {draft["id"]}\nDraft message: {draft["message"]}')
+    except HttpError as error:
+        print(f"HTTP Error occurred: {error}")
+        exit(2)
+
+    return draft
+
+
+def verify_email():
+    pass
+
+
+def send_email():
+    pass
 
 
 def main():
@@ -38,10 +73,13 @@ def main():
         service = build("gmail", "v1", credentials=creds)
 
         # TODO: Compose a draft email, verify with user, send email.
+        message_text = "This is text for a test message."
+        create_draft_email(service, message_text)
 
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
         print(f"An error occurred: {error}")
+        exit(1)
 
 
 if __name__ == "__main__":
