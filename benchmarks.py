fbddbfb561a22a972aa8c70fb9eb5f244f9fabf5
fadilm777 <fadil.m777@gmail.com>
2023-09-20 18:12:46 -0600

    Added test images to yolov5 and setup benchmarks

--- benchmarks.py
+++ benchmarks.py
@@ -1,4 +1,5 @@
 import time
+import os
 import sys
 from image_analysis.inference_yolov5 import setup_ml, analyze_img
 
@@ -14,13 +15,25 @@ This test analyzed 0.png using our machine learning model
 It should return a blue landing pad
 """
 
-img_path = "./tests/pytorch_yolov5_image_inference/0.png"
+i = 1    
+while True:    
 
-# Start a timer for recording inference time
-t = time.time()
+    if os.path.isfile(f"./tests/pytorch_yolov5_image_inference/image ({i}).JPG"):
+        img_path = f"./tests/pytorch_yolov5_image_inference/image ({i}).JPG"
+    elif os.path.isfile(f"./tests/pytorch_yolov5_image_inference/image ({i}).jpeg"):
+        img_path = f"./tests/pytorch_yolov5_image_inference/image ({i}).jpeg"
+    elif os.path.isfile(f"./tests/pytorch_yolov5_image_inference/image ({i}).png"):
+        img_path = f"./tests/pytorch_yolov5_image_inference/image ({i}).png"
+    else: break
+        
+            
+    # Start a timer for recording inference time
+    t = time.time()
 
-results = analyze_img(path=img_path,
-                        model=model,
-                        imgsz=imgsz)
+    results = analyze_img(path=img_path,
+                            model=model,
+                            imgsz=imgsz)
 
-print(f"Inference Time: {time.time() - t : .3f} secs")
+    print(f"Inference Time: {time.time() - t : .3f} secs")
+    i+=1
+print("images tested:", i-1)
