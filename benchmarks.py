3f05a240388aeb10ce1796134b8b0ee65884b461
fadilm777 <fadil.m777@gmail.com>
2023-09-27 16:14:31 -0600

    Update benchmarks.py

--- benchmarks.py
+++ benchmarks.py
@@ -1,19 +1,12 @@
 import time
 import os
-import sys
 from image_analysis.inference_yolov5 import setup_ml, analyze_img
 
 
-model, imgsz = setup_ml(weights="./tests/pytorch_yolov5_image_inference/landing_nano.pt",
-                            imgsz=(512, 512),
-                            device='cpu')
+model, imgsz = setup_ml(weights="./tests/pytorch_yolov5_image_inference/landing_nano.pt", imgsz=(512, 512), device='cpu')
 
 
-"""
-This test analyzed 0.png using our machine learning model
 
-It should return a blue landing pad
-"""
 
 i = 1    
 while True:    
@@ -24,16 +17,10 @@ while True:
         img_path = f"./tests/pytorch_yolov5_image_inference/image ({i}).jpeg"
     elif os.path.isfile(f"./tests/pytorch_yolov5_image_inference/image ({i}).png"):
         img_path = f"./tests/pytorch_yolov5_image_inference/image ({i}).png"
-    else: break
-        
-            
+    else: break        
     # Start a timer for recording inference time
     t = time.time()
-
-    results = analyze_img(path=img_path,
-                            model=model,
-                            imgsz=imgsz)
-
-    print(f"Inference Time: {time.time() - t : .3f} secs")
-    i+=1
-print("images tested:", i-1)
+    results = analyze_img(path = img_path, model = model, imgsz = imgsz)
+    print(f"Inference Time: {(time.time() - t)*1000 : .3f} milliseconds")
+    i += 1
+print("images tested:", i - 1)
