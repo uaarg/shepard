795fee0939663385326536e061fe61021ee9f8de
fadilm777 <fadil.m777@gmail.com>
2023-09-27 16:31:06 -0600

    lint fix

--- benchmarks.py
+++ benchmarks.py
@@ -2,14 +2,10 @@ import time
 import os
 from image_analysis.inference_yolov5 import setup_ml, analyze_img
 
-
 model, imgsz = setup_ml(weights="./tests/pytorch_yolov5_image_inference/landing_nano.pt", imgsz=(512, 512), device='cpu')
 
-
-
-
-i = 1    
-while True:    
+i = 1
+while True:
 
     if os.path.isfile(f"./tests/pytorch_yolov5_image_inference/image ({i}).JPG"):
         img_path = f"./tests/pytorch_yolov5_image_inference/image ({i}).JPG"
@@ -17,7 +13,8 @@ while True:
         img_path = f"./tests/pytorch_yolov5_image_inference/image ({i}).jpeg"
     elif os.path.isfile(f"./tests/pytorch_yolov5_image_inference/image ({i}).png"):
         img_path = f"./tests/pytorch_yolov5_image_inference/image ({i}).png"
-    else: break        
+    else: 
+        break
     # Start a timer for recording inference time
     t = time.time()
     results = analyze_img(path = img_path, model = model, imgsz = imgsz)
