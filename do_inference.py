e5c5595ce58363ccfb25d85711d7f9b71b95152a
Bradley Bravender <bsbraven@ualberta.ca>
2023-10-07 22:37:12 -0600

    Pushing do_inference.py to do_inference branch

---
+++ do_inference.py
@@ -0,0 +1,18 @@
+import cv2
+import sys
+from image_analysis.inference_yolov5 import setup_ml, annotate_img, analyze_img
+# program runs from command line and takes an image file argument which it runs YOLOv5 to annotate
+model, imgsz = setup_ml()
+
+try:
+    filename = str(sys.argv[1])
+    inference = analyze_img(filename,model)
+    annotated_image = annotate_img(filename, inference)
+    cv2.imwrite("result.png", annotated_image)
+except FileNotFoundError:
+    print()
+    print("Use the tab key to ensure your filepath is correct")
+except IndexError:
+    print()
+    print("Program must be run from command line in the form 'python3 command_line_annotator.py <filepath>'")
+
