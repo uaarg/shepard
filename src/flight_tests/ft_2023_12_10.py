83c82c8100335e6214335c49792e8f8915ecfd6d
Uzair Tariq <uzair@wrongsyntax.dev>
2023-12-10 10:49:03 -0700

    small fix

--- src/flight_tests/ft_2023_12_10.py
+++ src/flight_tests/ft_2023_12_10.py
@@ -14,37 +14,38 @@ nav = navigator.Navigator(drone, conn_str)
 lander = lander.Lander()
 
 while not (drone.armed and drone.mode == VehicleMode("GUIDED")):
+    pass
 
-    nav.takeoff(20)
-    drone.groundspeed = 5  # m/s
+nav.takeoff(20)
+drone.groundspeed = 5  # m/s
 
-    # Fly a square with 50 m side lengths
-    nav.set_position_relative(0, -50)
-    nav.set_position_relative(50, 0)
-    nav.set_position_relative(0, 50)
-    nav.set_position_relative(-50, 0)
+# Fly a square with 50 m side lengths
+nav.set_position_relative(0, -50)
+nav.set_position_relative(50, 0)
+nav.set_position_relative(0, 50)
+nav.set_position_relative(-50, 0)
 
-    time.sleep(5)
+time.sleep(5)
 
-    # Test altitude adjustments
-    nav.set_altitude_relative(10)
-    nav.set_altitude_relative(-10)
-    nav.set_altitude(30)
-    nav.set_altitude(20)
+# Test altitude adjustments
+nav.set_altitude_relative(10)
+nav.set_altitude_relative(-10)
+nav.set_altitude(30)
+nav.set_altitude(20)
 
-    time.sleep(5)
+time.sleep(5)
 
-    # Test heading adjustments
-    nav.set_heading_relative(90)
-    nav.set_heading_relative(-90)
-    nav.set_heading(0)
-    nav.set_heading(180)
+# Test heading adjustments
+nav.set_heading_relative(90)
+nav.set_heading_relative(-90)
+nav.set_heading(0)
+nav.set_heading(180)
 
-    time.sleep(5)
+time.sleep(5)
 
-    # Test landing pad search sequence
-    lander.generateRoute()
-    lander.goNext(nav, lander.route, drone.location.global_relative_frame.alt)
+# Test landing pad search sequence
+lander.generateRoute()
+lander.goNext(nav, lander.route, drone.location.global_relative_frame.alt)
 
-    # RTL
-    nav.return_to_launch()
+# RTL
+nav.return_to_launch()
