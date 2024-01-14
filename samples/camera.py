96d865a910bdfeab6dfaf6726b447eb95d3e447b
Aidan Olsen <a.olsen2024@icloud.com>
2024-01-14 13:46:31 -0700

    Create camera sample script

---
+++ samples/camera.py
@@ -0,0 +1,23 @@
+# Test the WebcamCamera class
+
+import time
+import sys
+
+from src.modules.imaging.camera import WebcamCamera
+
+cam = WebcamCamera()
+
+print("Taking a picture in 3s.")
+
+for i in reversed(range(3)):
+    print(f"{i + 1} ", end="")
+    # stdout is linebuffered, so nothing will print until a \n otherwise
+    sys.stdout.flush()
+
+    time.sleep(1)
+
+print("\rSay cheese!")
+time.sleep(1)
+
+cam.caputure_to("you.png")
+print("Image saved to you.png")
