42aecdfb67ea6e9f76f1d71115c53f1fa37a0896
Aidan Olsen <a.olsen2024@icloud.com>
2024-02-05 12:33:30 -0700

    Add type check script

---
+++ scripts/type.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+export PYTHONPATH=".:dep/labeller:dep/labeller/third_party/yolov5"
+
+echo "Checking imaging modules"
+mypy --check-untyped-defs --ignore-missing-imports src/modules/imaging/*.py
+
+echo "Checking tests"
+mypy test/*.py
