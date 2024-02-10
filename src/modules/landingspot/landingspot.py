356f2279fbdd90f5d005b85c1d59ddd46e7d7c97
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-02-10 16:04:40 -0700

    new changes

--- src/modules/landingspot/landingspot.py
+++ src/modules/landingspot/landingspot.py
@@ -1,37 +1,90 @@
-#let the present location be (0,0)
-#we pass in the length of the side of the square cam area
-#we pass in the number of loops around the origin, and has a default value of 20
+# #let the present location be (0,0)
+# #we pass in the length of the side of the square cam area
+# #we pass in the number of loops around the origin, and has a default value of 20
 
         
-def landingspot(a,number_of_loops=20):
-    route=[]
-    x,y=0,0
-    for i in range(2,number_of_loops,2):
+# def landingspot(a,number_of_loops=20):
+#     route=[]
+#     x,y=0,0
+#     for i in range(2,number_of_loops,2):
         
-        for j in range(4):
-            if j==0:
-                y=y-a
-                route.append([x,y])
-                for k in range(i-1):
-                    x=x+a
-                    route.append([x,y])
-            if j==1:
-                for k in range(i):
-                    y=y+a
-                    route.append([x,y])
-            if j==2:
-                for k in range(i):
-                    x=x-a
-                    route.append([x,y])
-            if j==3:
-                for k in range(i):
-                    y=y-a
-                    route.append([x,y])
-    print(route)
+#         for j in range(4):
+#             if j==0:
+#                 y=y-a
+#                 route.append([x,y])
+#                 for k in range(i-1):
+#                     x=x+a
+#                     route.append([x,y])
+#             if j==1:
+#                 for k in range(i):
+#                     y=y+a
+#                     route.append([x,y])
+#             if j==2:
+#                 for k in range(i):
+#                     x=x-a
+#                     route.append([x,y])
+#             if j==3:
+#                 for k in range(i):
+#                     y=y-a
+#                     route.append([x,y])
+#     print(route)
 
-landingspot(1,15)
+# landingspot(1,15)
 
             
                 
-        
+def process_imaging_data(imaging_data):
+    # Process imaging data to extract relevant information
+    # For example, identify potential landing spots, obstacles, etc.
+    # Return coordinates of landing spots or relevant information
+    pass
+
+def is_safe_to_land(x, y, imaging_data):
+    # Implement logic to determine if it's safe to land at the given coordinates
+    # Check for obstacles, terrain conditions, etc. based on imaging data
+    # Return True if safe, False otherwise
+    pass
+
+def landingspot(a, imaging_data, number_of_loops=20):
+    # Process imaging data to extract relevant information
+    landing_spots = process_imaging_data(imaging_data)
+
+    # Iterate over potential landing spots
+    for spot in landing_spots:
+        x, y = spot
+        if is_safe_to_land(x, y, imaging_data):
+            print("Landing at:", spot)
+            return
+        else:
+            print("It's not safe to land at:", spot)
+    
+    # If no safe landing spots found, fallback to default behavior
+    print("No safe landing spots found. Using default landing pattern:")
+    route = []
+    x, y = 0, 0
+    for i in range(2, number_of_loops, 2):
+        for j in range(4):
+            if j == 0:
+                y = y - a
+                route.append([x, y])
+                for k in range(i - 1):
+                    x = x + a
+                    route.append([x, y])
+            if j == 1:
+                for k in range(i):
+                    y = y + a
+                    route.append([x, y])
+            if j == 2:
+                for k in range(i):
+                    x = x - a
+                    route.append([x, y])
+            if j == 3:
+                for k in range(i):
+                    y = y - a
+                    route.append([x, y])
+    print(route)
+
+# Example usage
+imaging_data = [...]  # Imaging data obtained from another source
+landingspot(1, imaging_data, 15)
 
