d9c570e1c81baf11bcb0e81b553fb2e05e14acc1
Raihan <rfshaikh@ualberta.ca>
2024-02-10 17:16:48 -0700

    hide button added

--- src/modules/imaging/debug.py
+++ src/modules/imaging/debug.py
@@ -17,8 +17,8 @@ class ImageAnalysisDebugger:
         self.image: Optional[Image.Image] = None
         self.root = tk.Tk()
         self.is_visible = False
-        self.hide_button = tk.Button(text ='Hide Window',command = self.hide)
-        self.hide_button.place(x=10,y=10)
+        self.hide_button = tk.Button(text ='Hide Window', command = self.hide)
+        self.hide_button.place(x=10, y=10)
 
     def show(self):
         """
