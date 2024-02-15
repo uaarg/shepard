53a9c6c71e3f84bc2bd7bfd7b1dce80b75c6d40f
Aidan Olsen <arolsen@ualberta.ca>
2024-02-15 07:57:55 -0700

    make python script cache

--- install/openhd-python.sh
+++ install/openhd-python.sh
@@ -6,15 +6,19 @@ set -x
 echo "Installing wget"
 sudo apt install wget
 
-echo "Downloading Python3.10"
-wget https://www.python.org/ftp/python/3.10.13/Python-3.10.13.tgz
+if ! [[ -f Python-3.10.13.tgz ]]; then
+  echo "Downloading Python3.10"
+  wget https://www.python.org/ftp/python/3.10.13/Python-3.10.13.tgz
+fi
 
 echo "Installing Python SSL Modules"
 sudo apt install libssl-dev libxslt-dev
 #libncurses5-dev libsqlite3-dev libreadline-dev libtk8.6 libgdm-dev libdb4o-cil-dev libpcap-dev
 
 echo "Untarring Python file"
-tar xf Python-3.10.13.tgz
+if ! [[ -d Python-3.10.13 ]]; then
+  tar xf Python-3.10.13.tgz
+fi
 
 echo "Installing Python"
 cd Python-3.10.13/
