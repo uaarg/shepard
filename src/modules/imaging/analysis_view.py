92ff49dfd350ecd108417d59daa63c8878521025
fadilm777 <fadil.m777@gmail.com>
2024-03-01 05:21:38 -0700

    lint fix

--- src/modules/imaging/analysis_view.py
+++ src/modules/imaging/analysis_view.py
@@ -2,6 +2,7 @@ from mavlink import MAVLinkDelegate
 import pymavlink.dialects.v20.all as dialect
 from dep.labeller.benchmarks.detector import BoundingBox
 
+
 class AnalysisView:
 
     """
@@ -9,9 +10,9 @@ class AnalysisView:
 
     TODO: send a debugging message of desired type.
     """
-    
+
     def __init__(self, mavlinkDelegate: MAVLinkDelegate) -> None:
-        
+  
         self.mavlink = mavlinkDelegate
 
     def send_BoundingBox(self, bbox: BoundingBox) -> None:
@@ -19,9 +20,10 @@ class AnalysisView:
         position = bbox.position
         size = bbox.size
         values = [position.x, position.y, size.x, size.y]
-        for i in range(4,58): values[i] = 0.0
+        for i in range(4, 58): 
+            values[i] = 0.0
         dbg_box = dialect.MAVLink_debug_float_array_message(name=bytes("dbg_box", 'utf-8'),
-                                                                time_usec=0, #timestamp
-                                                                 array_id=0,
-                                                                 data=values)
+                                                            time_usec=0,  # timestamp
+                                                            array_id=0,
+                                                            data=values)
         self.mavlink.send(dbg_box)
