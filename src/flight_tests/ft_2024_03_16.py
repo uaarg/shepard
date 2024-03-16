a8239ff83876fdc9fe52333240f7d0d76e20e90e
Uzair Tariq <uzair@wrongsyntax.dev>
2024-03-16 09:42:51 -0600

    Fixes to navigator, and final flight script

--- src/flight_tests/ft_2024_03_16.py
+++ src/flight_tests/ft_2024_03_16.py
@@ -22,7 +22,8 @@ nav.send_status_message("Executing mission")
 time.sleep(2)
 
 nav.takeoff(10)
-start_coords = drone.location.global_relative_frame
+drone.groundspeed = 2  # m/s
+# start_coords = drone.location.global_relative_frame
 time.sleep(2)
 
 nav.set_altitude_position_relative(-10, 0, 10)
@@ -31,13 +32,21 @@ time.sleep(1)
 nav.set_position_relative(0, -10)
 time.sleep(1)
 
-lander.generateRoute(2)
+nav.send_status_message("Executing landing pad search")
+lander.generateRoute(4)
 
 for route in lander.route:
-    lander.goNext(nav, route, 5)
+    lander.goNext(nav, route, 10)
     time.sleep(3)
 
-nav.set_position(start_coords.lat, start_coords.lon)
-time.sleep(1)
+# nav.set_position(start_coords.lat, start_coords.lon)
+# time.sleep(1)
+# nav.land()
 
-nav.land()
+nav.return_to_launch()
+
+drone.close()
+
+nav.send_status_message("Flight test script execution terminated")
+
+
