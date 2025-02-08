7c2050b8b259e3de58fb7c0ec70536ba96b328c2
Uzair Tariq <uzair@wrongsyntax.dev>
2025-02-08 14:57:31 -0700

    Fix NoneType access error

--- samples/xm125_polling.py
+++ samples/xm125_polling.py
@@ -2,12 +2,16 @@ from time import sleep
 
 import src.modules.autopilot.altimeter as altimeter
 
-altimeter = altimeter.XM125()
+altimeter = altimeter.XM125(average_window=5)
 altimeter.begin()
 
 while True:
     result = altimeter.measure()
     if result:
-        print("Average dist: ", result[0]['averaged'][0])
+        average = result[0]['averaged']
+
+        # Average distance will not be available until at least `average_window` measurements have been taken
+        if average:
+            print("Average dist: ", result[0]['averaged'][0])
 
     sleep(0.5)
