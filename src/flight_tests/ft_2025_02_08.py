d20e2af39e257cfd8d6710334e6ee1495a925450
UA-24 X500 Pi <uaarg@gmail.com>
2025-02-08 14:00:52 -0700

    imaging script

--- src/flight_tests/ft_2025_02_08.py
+++ src/flight_tests/ft_2025_02_08.py
@@ -39,7 +39,7 @@ def take_picture(_):
     return (f"tmp/log/{ft_num}/{i}.png")
 
 
-def predict(pathToImage: str)
+def predict(pathToImage: str):
     image = Image.open(pathToImage)
     results = model.predict(image)
     with open('tmp/2025_02_08.log', 'w') as file:
@@ -54,66 +54,3 @@ while True:
 #mavlink.subscribe(take_picture)
 #mavlink.run()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Last login: Fri Feb  7 01:40:43 on ttys004
-❯ cd Desktop
-❯ clear
-import PIL.Image
-import PIL.Image
-import numpy as np
-from ultralytics import YOLO
-from PIL import Image
-model = YOLO("yolov10n.pt")
-import copy
-
-# Perform object detection on an image
-
-
-
-
-image = np.array(Image.open('catandsuitcase2.jpg').convert("RGB"))
-
-
-
-results = model(image) # This prints two lines
-
-
-class_names = model.names  # Dictionary mapping class IDs to object names
-
-detected_objects = []  # List to store results
-
-for result in results:
-    if hasattr(result, "boxes") and result.boxes is not None:
-        class_ids = result.boxes.cls.cpu().numpy()  # Get class IDs
-        boxes = result.boxes.xyxy.cpu().numpy()  # Get bounding box coordinates
-
-        # Create a list of tuples (object_name, bounding_box)
-
-        detected_objects = [
-            (class_names[int(cls_id)], box.tolist())
-            for cls_id, box in zip(class_ids, boxes)
-        ]
-
-
-result_dict = {}
-
-for obj_name, bbox in detected_objects:
-    print(f"Detected: {obj_name}, BBox: {bbox}")
-
-    result_dict[obj_name] = copy.deepcopy(result_dict[obj_name] + [bbox]) if (obj_name in result_dict) else [bbox]
-
-
-print(result_dict)
