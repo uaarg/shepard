e8d0395843ca4a7799dd749023e82e9cdc042773
Liyu2001 <uaarg@UAARG-Main>
2025-03-01 13:39:27 -0700

    flight test 2025 march 1st

--- src/flight_tests/ft_camera_log.py
+++ src/flight_tests/ft_camera_log.py
@@ -1,13 +1,14 @@
 import time
 import os
+import threading
 
 from src.modules.imaging.camera import RPiCamera
-#from src.modules.imaging.mavlink import MAVLinkDelegate
-#from src.modules.imaging.location import MAVLinkLocationProvider
+from src.modules.imaging.mavlink import MAVLinkDelegate
+from src.modules.imaging.location import MAVLinkLocationProvider
 
 cam = RPiCamera()
-#mavlink = MAVLinkDelegate()
-#location = MAVLinkLocationProvider(mavlink)
+mavlink = MAVLinkDelegate()
+location = MAVLinkLocationProvider(mavlink)
 
 os.makedirs("tmp/log", exist_ok=True)
 dirs = os.listdir("tmp/log")
@@ -28,14 +29,17 @@ def take_picture(_):
     last_picture = now
 
     cam.caputure_to(f"tmp/log/{ft_num}/{i}.png")
-    #location.dump_to(f"tmp/log/{ft_num}/{i}.json")
+    location.dump_to(f"tmp/log/{ft_num}/{i}.json")
     print(i)
     i += 1
 
+def picture_loop(sleep):
+    while True:
+        take_picture(None)
+        time.sleep(sleep)
+thread_1 = threading.thread(picture_loop, 3)
 
-while True:
-    take_picture(None)
-    time.sleep(1 / 15)
+thread_1.start()
 
-#mavlink.subscribe(take_picture)
-#mavlink.run()
+mavlink.run()
+
