f5bb71f7510f6e63dc78c60346b935316a0e0ea9
Kaiden Keane <kjkeane88@gmail.com>
2025-03-22 10:54:21 -0600

    balloon search

--- src/flight_tests/balloon_search_2025_03_08.py
+++ src/flight_tests/balloon_search_2025_03_08.py
@@ -63,14 +63,22 @@ try:
     dirs = os.listdir("tmp/log")
     ft_num = len(dirs)
 
-    while True:
+    prev_movement_dir = None
+    prev_movement_amnt = 60
+    turn_count = 0
+
+    while turn_count < 6: # 60 degrees * 6 = 360 => we completed a full circle without finding balloons
         step_size = 1  # meters
         last_pic = current_pic
         distance, direction, current_pic = detector.process_image_directory(directory_path=f"tmp/log/{ft_num}")
 
         if current_pic == last_pic: continue
+        
+        
 
-        if direction is not None:
+        if direction is not None: # we saw balloon
+            prev_movement_dir = direction # keep track in case balloon goes out of frame after previously found
+            prev_movement_amnt //= 2
             nav.send_status_message(f"Balloon detected: Move {direction}, Distance: {distance:.2f}")
 
             if direction == "center":
@@ -79,14 +87,28 @@ try:
                 metres_north_relative = step_size * math.sin(math.radians(current_heading))
                 metres_east_relative = step_size * math.cos(math.radians(current_heading))
                 nav.set_position_relative(metres_north_relative, metres_east_relative)
-            elif direction == "right":
-                nav.set_heading_relative(10)
-            elif direction == "left":
-                nav.set_heading_relative(-10)
 
-        else:
-            nav.send_status_message("No balloons detected")
-            nav.set_heading_relative(10)
+            # best thing we could do is calculate the angle required to centre it
+            # could also keep halving the distance
+            elif direction == "right":
+                nav.set_heading_relative(prev_movement_amnt)
+            elif direction == "left":
+                nav.set_heading_relative(-prev_movement_amnt)
+
+        else: # we did nto see the balloon
+            if prev_movement_dir == None:
+                turn_count += 1
+                nav.send_status_message("No balloons detected")
+                # fov is around 62 degrees so if we move 30 degrees either direction we get a (mostly) new image
+                nav.set_heading_relative(60)
+            else:
+                # we previously saw a balloon which we do not anymore
+                prev_movement_amnt //= 2
+                if prev_movement_dir == "right":
+                    nav.set_heading_relative(-prev_movement_amnt) # if previously moved right balloon would be to the left
+                else:
+                    nav.set_heading_relative(prev_movement_amnt)
+
 
 except KeyboardInterrupt:
     nav.send_status_message("Script interrupted by user")
