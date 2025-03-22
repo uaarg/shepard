66d422fed1e5ab847ff49786c051278755b59e19
Ritish Pentapalli <ritishpentapalli4@gmail.com>
2025-03-21 18:41:20 -0600

    Import typing

--- src/modules/imaging/detector.py
+++ src/modules/imaging/detector.py
@@ -7,6 +7,7 @@ import os
 import glob
 from dep.labeller.benchmarks.detector import LandingPadDetector, BoundingBox
 from dep.labeller.loader import Vec2
+from typing import Optional
 
 class BalloonDetector(LandingPadDetector):
     """
