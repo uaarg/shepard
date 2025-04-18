c869b674fb271d7f29d0f58597120a527b099ac7
fadilm777 <fadil.m777@gmail.com>
2025-04-18 12:32:36 -0600

    edits`

--- src/modules/imaging/detector.py
+++ src/modules/imaging/detector.py
@@ -16,11 +16,11 @@ class IrDetector(LandingPadDetector):
 
         gray_img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
         max_val = np.max(gray_img) # returns maximum value of brightness 
+        if max_val < 200: return None #lower threshold for intensity 
         _, thresh = cv2.threshold(gray_img, max_val - 10, 255, cv2.THRESH_BINARY)
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # cv2.drawContours(thresh, contours, -1, (0, 255, 0), 2)
-        cv2.imwrite("res.png", thresh)
 
         if len(contours) == 0: return None
 
