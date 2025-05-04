f9ff40bc94f70a9e1c76d242fbc08abe4dcb37b3
Fawwaz Hameed <fawwazgithub@gmail.com>
2025-05-03 18:37:36 -0600

    added more geofencing shit

---
+++ samples/geofence_test.py
@@ -0,0 +1,41 @@
+import json
+from src.modules.autopilot.lander import Lander
+from src.modules.georeference import inference_georeference
+
+with open('./samples/geofence.json', 'r') as f:
+    geofence = json.load(f)['features'][0]['geometry']['coordinates'][0]
+
+
+origin = [-113.54815575690603, 53.495546666117804]
+
+geofence = inference_georeference.Geofence_to_XY(origin, geofence)
+print(geofence)
+
+lander = Lander(0, 0)
+'''
+point1 = [53.4958, -113.5477]
+point2 = [53.4956, -113.5481]
+point3 = [53.4956, -113.5475]
+point4 = [53.4955, -113.5488]
+point5 = [53.4955, -113.5488]
+'''
+point1 = [0, 0]
+point2 = [5, 5]
+point3 = [10, 10]
+point4 = [-5, -5]
+point5 = [-10, 10]
+point6 = [-113.54802339060521,
+          53.49545124831053]
+point7 = [-113.54827877970348,
+          53.49573842746909]
+
+
+print(lander.geofence_check(geofence, point1))
+print(lander.geofence_check(geofence, point2))
+print(lander.geofence_check(geofence, point3))
+print(lander.geofence_check(geofence, point4))
+print(lander.geofence_check(geofence, point5))
+print(lander.geofence_check(geofence, point6))
+print(lander.geofence_check(geofence, point7))
+
+
