87d9d55561c40bad9802805b299e897a5ff5043e
fadilm777 <fadil.m777@gmail.com>
2026-02-14 11:57:57 -0700

    added consumer in emu

---
+++ samples/oakd.py
@@ -0,0 +1,56 @@
+from PIL import Image
+from src.modules.emu import Emu
+import time
+import json
+import threading
+
+from src.modules.imaging.camera import DepthCapture, OakdCamera
+
+emu = Emu("res")
+
+def print_conn():
+    print("connecton made")
+
+emu.set_on_connect(print_conn)
+latest_capture: DepthCapture | None = None
+
+emu.start_comms()
+time.sleep(2)
+
+camera = OakdCamera()
+
+camera_thread = threading.Thread(target=camera.start(), daemon=True)
+camera_thread.start()
+
+def send_img(message):
+    msg = json.loads(message)
+
+    if msg["type"] == "image" and msg["message"] == "capture":
+        print("sending image")
+        latest_capture = camera.capture()
+        im = Image.fromarray(latest_capture.rgb)
+
+        im.save("../tmp/sample.jpeg")
+        emu.send_image("../tmp/sample.jpeg")
+
+def measure(message):
+    msg = json.loads(message)
+
+    if msg["type"] == "getDistance":
+        points = msg["message"]
+
+        p1 = points["p1"]
+        p2 = points["p2"]
+
+        if latest_capture is not None:
+            distance = latest_capture.distance_between_points(p1["x"], p1["y"], p2["x"], p2["y"])
+            send = {
+                "type": "distance", 
+                "message": distance
+            }
+            emu.send_msg(json.dumps(send))
+
+
+emu.subscribe(send_img)
+emu.subscribe(measure)
+time.sleep(500)
--- src/modules/imaging/camera.py
+++ src/modules/imaging/camera.py
@@ -4,6 +4,8 @@ import pathlib
 from PIL import Image
 import numpy as np
 import cv2
+import depthai as dai
+from dataclasses import dataclass
 
 
 class CameraProvider:
@@ -40,6 +42,124 @@ class CameraProvider:
         """
         return np.array(self.capture())
 
+@dataclass
+class DepthCapture:
+    rgb: np.ndarray
+    point_cloud: np.ndarray
+    width: int
+    height: int
+
+    def get_point(self, x: int, y: int) -> np.ndarray:
+        """Get the 3D coordinates relative to the camera frame (in mm) of a pixel).
+
+        (x, y) are in pixel coordinates in the self.rgb frame."""
+        idx = y * self.width + x
+        p = self.point_cloud[idx]
+
+        # I am not sure if this is possible...
+        assert not np.any(np.isnan(p)), "Invalid depth at this point"
+
+        return p
+
+    def distance_between_points(self, x1: int, y1: int, x2: int, y2: int):
+        """Get the physical distance between points from pixels in the rgb
+        frame (x1, y1) and (x2, y2).
+
+        Resulting distance is in mm."""
+        p1 = self.get_point(x1, y1)
+        p2 = self.get_point(x2, y2)
+
+        dist = np.linalg.norm(p1 - p2)
+
+        return dist
+
+
+class OakdCamera(CameraProvider):
+    """
+    Manages an OAK-D device with on-demand capturing of 3D pictures (see DepthCapture).
+    """
+
+    def __init__(self, fps: int = 30):
+        self._init_pipeline(fps)
+
+    def _init_pipeline(self, fps: int):
+        """Initialize the Depth AI pipeline (will be run on the OAK-D)"""
+        pipeline = dai.Pipeline()
+
+        camRgb = pipeline.create(dai.node.ColorCamera)
+        monoLeft = pipeline.create(dai.node.MonoCamera)
+        monoRight = pipeline.create(dai.node.MonoCamera)
+        depth = pipeline.create(dai.node.StereoDepth)
+        pointcloud = pipeline.create(dai.node.PointCloud)
+        sync = pipeline.create(dai.node.Sync)
+        xOut = pipeline.create(dai.node.XLinkOut)
+
+        camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
+        camRgb.setIspScale(1, 3)
+        camRgb.setFps(fps)
+
+        monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+        monoLeft.setCamera("left")
+        monoLeft.setFps(fps)
+
+        monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+        monoRight.setCamera("right")
+        monoRight.setFps(fps)
+
+        depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+        depth.setLeftRightCheck(True)
+        depth.setSubpixel(True)
+        depth.setDepthAlign(dai.CameraBoardSocket.CAM_A)
+
+        monoLeft.out.link(depth.left)
+        monoRight.out.link(depth.right)
+        depth.depth.link(pointcloud.inputDepth)
+
+        camRgb.isp.link(sync.inputs["rgb"])
+        pointcloud.outputPointCloud.link(sync.inputs["pcl"])
+
+        sync.out.link(xOut.input)
+        xOut.setStreamName("out")
+
+        self.pipeline = pipeline
+
+    def capture_with_depth(self) -> DepthCapture:
+        """Capture a current 3D frame on the OAK-D.
+
+        NOTE: .start() must have been called first. If it has not, this will raise Exception."""
+        if not self.device or self.device.isClosed():
+            raise Exception("No oakD connection, perhaps you forgot to call the .start() function")
+
+        msg = self.queue.get()
+        rgbFrame = msg["rgb"]
+        cv_frame = rgbFrame.getCvFrame()
+        rgb_frame = cv2.cvtColor(cv_frame, cv2.COLOR_BGR2RGB)
+        rgb = np.array(rgb_frame)
+        pcl = msg["pcl"]
+
+        point_cloud = pcl.getPoints().astype(np.float64)
+        height, width, _ = rgb.shape
+
+        capture = DepthCapture(rgb, point_cloud, width, height)
+        return capture
+
+    def capture(self) -> Image.Image:
+        capture = self.capture_with_depth
+        img = Image.fromarray(capture.rgb, "RGB")
+        return img
+
+
+    def start(self):
+        """Start the depth-perception process on the OAK-D"""
+        print("Starting OAK-D Connection")
+        self.device = dai.Device(self.pipeline)
+        self.queue = self.device.getOutputQueue("out", maxSize=1, blocking=False)
+
+    def stop(self):
+        """Stop the depth-perception process"""
+        self.device.close()
+        self.queue = None
 
 class DebugCamera(CameraProvider):
     """
