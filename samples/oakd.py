fddceee7e6fef93fc5a668a9305e9168c168b70b
Manan Arya <aryamanan1@gmail.com>
2026-07-10 15:03:39 +0530

    as requested

--- samples/oakd.py
+++ samples/oakd.py
@@ -33,7 +33,6 @@ camera = OakdCamera()
 camera_thread = threading.Thread(target=camera.start(), daemon=True)
 camera_thread.start()
 
-
 def send_img(message):
     global latest_capture, i, ft_num
 
